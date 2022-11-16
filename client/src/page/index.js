7df91b036fa0cf2f117eb0a27dc633869cc89cb8
Ambareen09 <ambubushra@gmail.com>
2022-11-16 18:12:22 +0530

    `player info dynamic`

--- client/src/page/index.js
+++ client/src/page/index.js
@@ -2,5 +2,6 @@ import Home from './Home'
 import CreateBattle from './CreateBattle'
 import JoinBattle from './JoinBattle'
 import Battle from './Battle'
+import Battleground from './Battleground'
 
-export { Home, CreateBattle, JoinBattle, Battle }
+export { Home, CreateBattle, JoinBattle, Battle, Battleground }
